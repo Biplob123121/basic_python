919b453eaff7a225f4fc57cdbc243afaa0cbb1bb
Biplob123121 <biplobkb123@gmail.com>
2024-05-21 00:07:18 +0600

    discussing datatypes

--- data_types.py
+++ data_types.py
@@ -1,5 +1,5 @@
 
-# first block ------------All types------------>
+# first block ------------All types---------------------------->
 
 # text type = str
 # Numeric types = int, float, complex
@@ -10,6 +10,36 @@
 # Binary types = bytes, bytearray, memoryview
 # None type = NoneType
 
+#Advance type = Decorators, Generators, Iterators, MetaProgramming
 
 
-# second block ---------------
+
+# second block ---------------Number type-------------------------->
+quantity = 5     #int
+price = 5.50     #float
+z = 3+4j         #complex
+
+
+#third block ---------- string type -------------------------------->
+
+my_name = "Biplob"
+country = "Bangladesh"
+
+
+# fourth block --------------------Sequence type --------------------->
+# list 
+my_list = [1, 2, 3]
+another_list = [1, 2, [3, "three"]]
+
+#tuple
+my_tuple = (1, "two", 3.5)
+
+# dictionary
+my_dict = {'food': 'rice', 'price': 10}
+
+# Set type
+my_set = {"a", "b", "c"}
+
+
+
+# fifth block -----------------------Advance type------------------->
