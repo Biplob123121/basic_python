f40eece74d4db543b4e8ad9ec835c903407ba035
Biplob123121 <biplobkb123@gmail.com>
2024-05-20 23:39:55 +0600

    first commit

---
+++ data_types.py
@@ -0,0 +1,15 @@
+
+# first block ------------All types------------>
+
+# text type = str
+# Numeric types = int, float, complex
+# Sequence types = list, tuple, range
+# Mapping types = dict
+# Set types = set, frozenset
+# Boolean types = bool
+# Binary types = bytes, bytearray, memoryview
+# None type = NoneType
+
+
+
+# second block ---------------
---
+++ hello.py
@@ -0,0 +1,3 @@
+print("Hello Python")
+
+print("It is time to play with python..")
